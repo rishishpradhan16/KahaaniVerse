6cd28041f71d1b3cb70e479c0660d1c4122297ce
rishishpradhan16 <rishish.pradhan@gmail.com>
2025-09-12 00:35:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,33 +11,39 @@ import Contact from "./pages/Contact";
 import Privacy from "./pages/Privacy";
 import Disclaimer from "./pages/Disclaimer";
 import NotFound from "./pages/NotFound";
+import { useForceBackHome } from "./hooks/useForceBackHome"; // 👈 new hook
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <MobileNavProvider>
-      <BookProvider>
-      <TooltipProvider>
-        <div className="dark">
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/privacy" element={<Privacy />} />
-              <Route path="/disclaimer" element={<Disclaimer />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
-      </TooltipProvider>
-    </BookProvider>
-    </MobileNavProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  // 👇 yeh line se back button globally override hoga
+  useForceBackHome();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MobileNavProvider>
+        <BookProvider>
+          <TooltipProvider>
+            <div className="dark">
+              <Toaster />
+              <Sonner />
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Index />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="/privacy" element={<Privacy />} />
+                  <Route path="/disclaimer" element={<Disclaimer />} />
+                  {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </BrowserRouter>
+            </div>
+          </TooltipProvider>
+        </BookProvider>
+      </MobileNavProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
